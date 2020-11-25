200ca1e857865382ac1d13d2ca48c350a7e254db
Nonantiy <45326482+Nonanti@users.noreply.github.com>
2020-11-25 11:16:13 +0300

    Add files via upload

---
+++ Unturned-UI-Plugin/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Logo.UI.Config;
+namespace Unturned_UI_Plugin
+{
+    public class Plugin : RocketPlugin<Config>
+    {
+        public static Plugin Instance;
+        protected override void Load()
+        {
+            Instance = this;
+            U.Events.OnPlayerConnected += oyuncubaglandı;
+        }
+        private void oyuncubaglandı(UnturnedPlayer player)
+        {
+            EffectManager.sendUIEffect(this.Configuration.Instance.UI, (short)5924, player.CSteamID, true);
+            EffectManager.sendUIEffectImageURL(5924, player.CSteamID, true, "Resim", this.Configuration.Instance.URL);
+            EffectManager.sendUIEffect(Configuration.Instance.UI, 31, player.CSteamID, true, Configuration.Instance.Sunucuİsim);
+        }
+        protected override void Unload()
+        {
+            base.Unload();
+            Instance = null;
+            U.Events.OnPlayerConnected -= oyuncubaglandı;
+        }
+    }
+}
