d3c19ad830d9ac150b8e8701c9415ec86860a8e6
Nonantiy <45326482+Nonanti@users.noreply.github.com>
2020-11-25 11:12:25 +0300

    Add files via upload

---
+++ Unturned-UI-Plugin/Config.cs
@@ -0,0 +1,17 @@
+﻿using Rocket.API;
+
+namespace Logo.UI.Config
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public ushort UI;
+        public string URL;
+        public string Sunucuİsim;
+        public void LoadDefaults()
+        {
+            UI = 5924;
+            URL = "";
+            Sunucuİsim = "";
+        }
+    }
+}
